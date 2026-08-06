63481ddbeb8d639b8fc6ba31231c3948edf9eb63
Vadim <vadim@example.com>
2026-08-06 20:24:46 +0300

    fix git oauth

--- config/home/git.nix
+++ config/home/git.nix
@@ -3,34 +3,38 @@
 {
   home = {
     stateVersion = "26.05";
+
+    packages = with pkgs; [
+      libsecret
+      git-credential-manager
+    ];
   };
 
   programs = {
     git = {
       enable = true;
-      userName = user.fullName;
-      userEmail = user.email;
       package = pkgs.gitFull;
 
-      aliases = {
-        st = "status";
-        ci = "commit";
-        co = "checkout";
-        br = "branch";
-        lg = "log";
-        df = "diff";
-        dc = "diff --cached";
-        hist = "log --graph --decorate --pretty=format:'%C(auto)%h%d %Cgreen(%ar)%Creset %C(bold blue)%an%Creset %s'";
-      };
-
       settings = {
+        user = {
+          name = user.fullName;
+          email = user.email;
+        };
+        alias = {
+          st = "status";
+          ci = "commit";
+          co = "checkout";
+          br = "branch";
+          lg = "log";
+          df = "diff";
+          dc = "diff --cached";
+          hist = "log --graph --decorate --pretty=format:'%C(auto)%h%d %Cgreen(%ar)%Creset %C(bold blue)%an%Creset %s'";
+        };
         credential = {
+          credentialStore = "secretservice";
           helper = [ "libsecret" ];
         };
       };
-    };
-    git-credential-oauth = {
-      enable = true;
     };
   };
 }
