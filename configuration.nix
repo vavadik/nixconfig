709ffee6c9cfeecc7a4839b9c34a6e7e68744642
Vadim Galanov <vgalanov@ascendix.com>
2026-08-06 14:26:58 +0300

    uncomment

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,6 @@
 { ... }:
-{ }
-# throw ''
-#   Missing NixOS configuration.nix!
-#   Generate or copy your existing configuration.nix to the root of the repository.
-# ''
+
+throw ''
+  Missing NixOS configuration.nix!
+  Generate or copy your existing configuration.nix to the root of the repository.
+''
