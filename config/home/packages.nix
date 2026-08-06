e2f0d119e00300efc9453b165e3e7c54817fc954
Vadim <vadim@example.com>
2026-08-06 23:18:24 +0300

    +claude

--- config/home/packages.nix
+++ config/home/packages.nix
@@ -8,11 +8,13 @@
       fzf
       lsd
       zoxide
+      claude-code
     ];
   };
   programs = {
     direnv = {
       enable = true;
+      silent = true;
       enableBashIntegration = true;
       enableFishIntegration = true;
       nix-direnv.enable = true;
