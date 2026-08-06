561e36cf4213ef47a06d67cefdad44339ca6b3d1
Vadim Galanov <vgalanov@ascendix.com>
2026-08-06 15:02:06 +0300

    git config

--- config/home/git.nix
+++ config/home/git.nix
@@ -11,7 +11,20 @@
 
   programs.git = {
     enable = true;
+    package = pkgs.gitFull;
     userName = user.fullName;
     userEmail = user.email;
+    aliases = {
+      st = "status";
+      ci = "commit";
+      co = "checkout";
+      br = "branch";
+      lg = "log";
+      df = "diff";
+      dc = "diff --cached";
+      hist = "log --graph --decorate --pretty=format:'%C(auto)%h%d %Cgreen(%ar)%Creset %C(bold blue)%an%Creset %s'";
+    };
   };
+
+  programs.git-credential-oauth.enable = true;
 }
